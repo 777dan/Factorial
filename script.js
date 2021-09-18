dbc4922f669bb5010f485cbbeb126d39bbf3105d
777dan <68803448+777dan@users.noreply.github.com>
2021-09-18 11:03:27 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+// let n = 10;
+// let i = 1;
+// while (i < 10) {
+//     document.write("=^.^=");
+//     i++
+// }
+
+// do {
+//     document.write("=^.^=");
+// } while (i < 10)
+
+// for (let i = 0; i < n; i++) {
+//     document.write("=^.^=");
+// }
+
+// let n = prompt("Введите число");
+// while (n > 1) {
+//     let n1 = n--;
+//     let n3 = n - 1;
+//     let n2 = n3 * n1;
+//     alert(n2);
+// }
+
+// let n = prompt("Введите число");
+// let s = 5;
+// while (n > 0) {
+//     s--;
+//     let n1 = n--;
+//     n2 = n * n1;
+//     // alert(n2);
+// }
+// alert(n2);
+
+let n = 5;
+let fact = 1;
+for (let i = 1; i <= n; i++) {
+    fact = fact * i;
+}
+alert(fact);
